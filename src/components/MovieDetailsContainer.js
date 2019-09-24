b100da05162d254818bb6908d5dd56657af8c9c6
gergokutu <gergo.kutu@gmail.com>
2019-09-24 17:26:02 +0200

    All movie data on the detail page

--- src/components/MovieDetailsContainer.js
+++ src/components/MovieDetailsContainer.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
 import MovieDetails from './MovieDetails'
+import { getMovies } from '../actions/movies'
 
 class MovieDetailsContainer extends Component {
+  state = {}
+
+  componentDidMount() {
+    this.props.getMovies()
+  }
+
   render() {
+    // console.log('MovieDetailsContainer PROPS:', this.props)
     return (
-      <MovieDetails />
+      <MovieDetails movies={this.props.movies}/>
     )
   }
 }
 
-export default MovieDetailsContainer
+function mapStateToProps(state) {
+  return {
+    movies: state.movies
+  }
+}
+
+// to be able to use getMovies in this component
+const mapDispatchToProps = {
+  getMovies
+} 
+
+export default connect(mapStateToProps, mapDispatchToProps)(MovieDetailsContainer)
