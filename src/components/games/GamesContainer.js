d309c2c3c6bf5802ab0b7deee2c43c696ecee591
gergokutu <gergo.kutu@gmail.com>
2019-09-29 13:44:10 +0200

    Styling » background-color, navbar in front

--- src/components/games/GamesContainer.js
+++ src/components/games/GamesContainer.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
+import '../../styling/games.css'
+
 class GamesContainer extends Component {
   render() {
     return (
