df32a2585012cfd7d3556ef69d6008b318af43bc
gergokutu <gergo.kutu@gmail.com>
2019-10-08 20:40:08 +0200

    Try pagination on frontend side

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,18 +1,18 @@
 import React from 'react'
 
-const Pagination = (props, totalPages, moviesPerPages, paginate) => {
+const Pagination = (props) => {
   console.log('props.movies:', props.movies)
   const { movies } = props
 
-  // const currentPage = 1
-  // const moviesPerPages = 6
-  // const totalPages = Math.ceil(props.movies.length / 6 )
-  // const paginate = 'D'
+  const currentPage = 1
+  const moviesPerPages = 6
+  const totalPages = props.movies.length
+  const paginate = 'D'
 
-  //  // get current post
-  //  const indexOfLastMovie = currentPage * moviesPerPages
-  //  const indexOfFirstMovie = indexOfLastMovie - moviesPerPages
-  //  const currentMovies = movies.slice(indexOfFirstMovie, indexOfLastMovie)
+   // get current post
+   const indexOfLastMovie = currentPage * moviesPerPages
+   const indexOfFirstMovie = indexOfLastMovie - moviesPerPages
+   const currentMovies = movies.slice(indexOfFirstMovie, indexOfLastMovie)
 
   const pageNumbers = []
 
